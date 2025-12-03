9995ddff52d27bd5c9075c548bd3b4d5ffe55266
PigDuck5390 <fprtm5390@naver.com>
2025-12-03 14:49:47 +0900

    fix

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+
 import MainHeader from './MainHeader.jsx'
 import firstMovie from '../img/주토피아.jpg'
 import secondMovie from '../img/주술회전.jpg'
@@ -8,8 +8,8 @@ import fourthMovie from '../img/정보원.jpg'
 import '../css/Main.css'
 
 function Main(){
-  const navigate = useNavigate();
 
+  
   return (
     <>
       <MainHeader />
@@ -36,21 +36,8 @@ function Main(){
 
                       11월, 초특급 추적 어드벤처가 펼쳐진다!
                       </span>
-                  <h1>주토피아 2</h1>
-                  <span>더 화려해진 세계, 더 넓어진 주토피아!
-                      디즈니의 가~~장 사랑스러운 콤비 '주디'와 '닉'이 돌아온다!
-
-                      미스터리한 뱀 ‘게리’가 나타난 순간,
-                      주토피아가 다시 흔들리기 시작했다!
-
-                      혼란에 빠진 도시를 구하기 위해
-                      환상의 콤비 ‘주디’ & ‘닉’이 잠입 수사에 나서고
-                      상상 그 이상의 진실과 위협을 마주하게 되는데...!
-
-                      11월, 초특급 추적 어드벤처가 펼쳐진다!
-                      </span>
                 </div>
-                <button className="quick-reserv" onClick={() => navigate('/booking/1')}>바로 예매하기</button>
+                <button className="quick-reserv">바로 예매하기</button>
               </div>
 
               <div className="movie-row">
@@ -58,10 +45,7 @@ function Main(){
                 <div className="desc">
                   <h1>영화제목</h1>
                   <span>2번 영화 설명 텍스트</span>
-                  <h1>영화제목</h1>
-                  <span>2번 영화 설명 텍스트</span>
                 </div>
-                <button className="quick-reserv">바로 예매하기</button>
                 <button className="quick-reserv">바로 예매하기</button>
               </div>
             </div>
@@ -72,10 +56,7 @@ function Main(){
                 <div className="desc">
                   <h1>영화제목</h1>
                   <span>3번 영화 설명 텍스트</span>
-                  <h1>영화제목</h1>
-                  <span>3번 영화 설명 텍스트</span>
                 </div>
-                <button className="quick-reserv">바로 예매하기</button>
                 <button className="quick-reserv">바로 예매하기</button>
               </div>
 
@@ -84,10 +65,7 @@ function Main(){
                 <div className="desc">
                   <h1>영화제목</h1>
                   <span>4번 영화 설명 텍스트</span>
-                  <h1>영화제목</h1>
-                  <span>4번 영화 설명 텍스트</span>
                 </div>
-                <button className="quick-reserv">바로 예매하기</button>
                 <button className="quick-reserv">바로 예매하기</button>
               </div>
             </div>
