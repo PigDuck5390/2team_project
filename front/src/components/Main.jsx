451d0afd440d3de801a965205194d7cdfe67b4a7
johyun123 <yjh0207m@naver.com>
2025-12-03 14:34:56 +0900

    메인 프론트 엔드 완성

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -23,17 +23,30 @@ function Main(){
               <div className="movie-row">
                 <img src={firstMovie} alt="1번 영화 포스터" />
                 <div className="desc">
-                  <h2>영화제목</h2>
-                  <p>1번 영화 설명 텍스트</p>
+                  <h1>주토피아 2</h1>
+                  <span>더 화려해진 세계, 더 넓어진 주토피아!
+                      디즈니의 가~~장 사랑스러운 콤비 '주디'와 '닉'이 돌아온다!
+
+                      미스터리한 뱀 ‘게리’가 나타난 순간,
+                      주토피아가 다시 흔들리기 시작했다!
+
+                      혼란에 빠진 도시를 구하기 위해
+                      환상의 콤비 ‘주디’ & ‘닉’이 잠입 수사에 나서고
+                      상상 그 이상의 진실과 위협을 마주하게 되는데...!
+
+                      11월, 초특급 추적 어드벤처가 펼쳐진다!
+                      </span>
                 </div>
+                <button className="quick-reserv">바로 예매하기</button>
               </div>
 
               <div className="movie-row">
                 <img src={secondMovie} alt="2번 영화 포스터" />
                 <div className="desc">
-                  <h2>영화제목</h2>
-                  <p>2번 영화 설명 텍스트</p>
+                  <h1>영화제목</h1>
+                  <span>2번 영화 설명 텍스트</span>
                 </div>
+                <button className="quick-reserv">바로 예매하기</button>
               </div>
             </div>
 
@@ -41,17 +54,19 @@ function Main(){
               <div className="movie-row">
                 <img src={thirdMovie} alt="3번 영화 포스터" />
                 <div className="desc">
-                  <h2>영화제목</h2>
-                  <p>3번 영화 설명 텍스트</p>
+                  <h1>영화제목</h1>
+                  <span>3번 영화 설명 텍스트</span>
                 </div>
+                <button className="quick-reserv">바로 예매하기</button>
               </div>
 
               <div className="movie-row">
                 <img src={fourthMovie} alt="4번 영화 포스터" />
                 <div className="desc">
-                  <h2>영화제목</h2>
-                  <p>4번 영화 설명 텍스트</p>
+                  <h1>영화제목</h1>
+                  <span>4번 영화 설명 텍스트</span>
                 </div>
+                <button className="quick-reserv">바로 예매하기</button>
               </div>
             </div>
           </div>
